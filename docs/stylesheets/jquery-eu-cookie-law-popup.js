2d6a8fa56ce3ca4c621169e416c2092dc73033d6
gbvitrano <gbvitrano@gmail.com>
2021-07-27 07:37:31 +0200

    upgrade site

--- docs/stylesheets/jquery-eu-cookie-law-popup.js
+++ docs/stylesheets/jquery-eu-cookie-law-popup.js
@@ -28,7 +28,7 @@ $.fn.euCookieLawPopup = (function() {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	// PARAMETERS (MODIFY THIS PART) //////////////////////////////////////////////////////////////
 	_self.params = {
-		cookiePolicyUrl : 'https://hfcqgis-md.readthedocs.io/it/latest/privacy/',
+		cookiePolicyUrl : 'https://opendatasicilia.github.io/HfcQGIS-md/privacy/',
 		popupPosition : 'top',
 		colorStyle : 'default',
 		compactStyle : false,
@@ -253,7 +253,7 @@ $(document).ready( function() {
 	if ($(".eupopup").length > 0) {
 		$(document).euCookieLawPopup().init({
 			'info' : 'YOU_CAN_ADD_MORE_SETTINGS_HERE',
-			'popupTitle' : '<img src="https://hfcqgis-md.readthedocs.io/it/latest/img/logo_hfc_white.svg" class="immagonobox"  alt="HfcQGIS" title="HfcQGIS - Help funzioni calcolatore di campi di QGIS" width="200" border="0" ><br><br> Questo sito, hfcqgis.opendatasicilia.it utilizza i cookie, anche di terze parti, per migliorare la vostra esperienza di navigazione web. <br> ',
+			'popupTitle' : '<img src="https://opendatasicilia.github.io/HfcQGIS-md/img/logo_hfc_white.svg" class="immagonobox"  alt="HfcQGIS" title="HfcQGIS - Help funzioni calcolatore di campi di QGIS" width="200" border="0" ><br><br> Questo sito, hfcqgis.opendatasicilia.it utilizza i cookie, anche di terze parti, per migliorare la vostra esperienza di navigazione web. <br> ',
 			'popupText' : 'Chiudendo questo messaggio, scorrendo questa pagina o cliccando su qualunque suo elemento acconsenti all&rsquo;uso dei cookie.<br> Per maggiori informazioni o per negare il consenso a tutti o ad alcuni cookie, consulta l&rsquo;informativa!</a>'
 		});
 	}
